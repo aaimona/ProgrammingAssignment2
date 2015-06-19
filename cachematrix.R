af1c97e1961eadbe766faa9939090a5fe46c4e21
Katarzyna <katarzyna.e.kudla@gmail.com>
2015-06-19 23:49:28 +0200

    Uploaded Assignment 2 solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions used together create a convenient way to invert a given matrix,
+## assuming that it is always invertible. 
 
-## Write a short comment describing this function
+## First function, makeCacheMatrix(), creates a wrapping object 
+## that represents the input matrix, cached solution and following functions:
+## - set, which sets the matrix for the input,
+## - get, which returns the input matrix,
+## - ss, which sets the solution (i.e. the solved matrix),
+## - gs, which gets the solution.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(mx = matrix()) {
+  
+  solution <- NULL
+  
+  set <- function(new_mx) {
+        mx <<- new_mx
+  }
+  
+  get <- function() mx
+  
+  set_solution <- function(new_solution) {
+        solution <<- new_solution
+  }
+  
+  get_solution <- function() solution
+  
+  
+  list(set = set, get = get, ss = set_solution, gs = get_solution)
+  
 }
 
 
-## Write a short comment describing this function
+## This function returns the inverted matrix which is done by:
+## (1) checking if a solution already exists, if so - returns it,
+## (2) if the solution is not yet computed, the function computes and returns it.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(a) {
+  
+  ex_sol <- a$gs()
+  
+  if(!is.null(ex_sol)) {
+        return(ex_sol)
+    
+  }
+  
+  cal_sol <- solve(a$get())
+  a$ss(cal_sol)
+  return(cal_sol)
+  
 }
